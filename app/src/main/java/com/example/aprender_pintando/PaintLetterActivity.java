c50c8da25fd9e8f72dacc7331df12bbd8c98eb49
femansilla <fmansilla@roemmers.com.ar>
2021-10-20 15:15:54 -0300

    recursos, controllers, activities

--- app/src/main/java/com/example/aprender_pintando/PaintLetterActivity.java
+++ app/src/main/java/com/example/aprender_pintando/PaintLetterActivity.java
@@ -1,20 +1,14 @@
 package com.example.aprender_pintando;
 
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class PaintLetterActivity extends AppCompatActivity {
+public class PaintLetterActivity  extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_paint_letter);
     }
 }
