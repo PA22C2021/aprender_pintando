f62bfce8282e7d5d0f7be11005a8b527be7ffee6
femansilla <fmansilla@roemmers.com.ar>
2021-10-30 11:09:39 -0300

    tratando de hacer la grilla con las letras parte 1

--- app/src/main/java/com/example/aprender_pintando/MainActivity.java
+++ app/src/main/java/com/example/aprender_pintando/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.aprender_pintando;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends BaseActivity  {
 
+    ImageButton btn_play, btn_continuar, btn_grilla, btn_config;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        btn_play = (ImageButton) findViewById(R.id.btn_play);
+        btn_continuar = (ImageButton) findViewById(R.id.btn_continue);
+        btn_grilla = (ImageButton) findViewById(R.id.btn_grid);
+        btn_config = (ImageButton) findViewById(R.id.btn_settings);
+
     }
     @Override
     protected int getLayoutResourceId(){
@@ -24,6 +32,12 @@ public class MainActivity extends BaseActivity  {
     public void ConfigurarJuego(View view)
     {
         Intent intent = new Intent(view.getContext(), SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    public void GrillaProgreso(View view)
+    {
+        Intent intent = new Intent(view.getContext(), ProgressGridActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/aprender_pintando/ProgressGridActivity.java
+++ app/src/main/java/com/example/aprender_pintando/ProgressGridActivity.java
@@ -1,12 +1,18 @@
 package com.example.aprender_pintando;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class ProgressGridActivity extends BaseActivity  {
+    GridView gridView;
+
+    int imgArray[] = {R.drawable.letra_a};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        gridView = findViewById(R.id.grilla_progreso);
     }
     @Override
     protected int getLayoutResourceId(){
