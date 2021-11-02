47f278c138279424e1352ac7f42cbd8f4bec33ab
femansilla <fmansilla@roemmers.com.ar>
2021-11-02 11:15:49 -0300

    sonidos - colores

--- app/src/main/java/com/example/aprender_pintando/SettingsActivity.java
+++ app/src/main/java/com/example/aprender_pintando/SettingsActivity.java
@@ -1,17 +1,58 @@
 package com.example.aprender_pintando;
 
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SettingsActivity extends BaseActivity  {
 
+
+    ImageView screen;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        screen = (ImageView) findViewById(R.id.template_screen);
+
     }
     @Override
     protected int getLayoutResourceId(){
         return R.layout.activity_settings;
+    }
+
+    public void setearSonido(View view){
+        int sonido = Integer.parseInt(view.getTag().toString());
+
+        if(rSonido != sonido){
+            sonidos[rSonido].stop();
+            sonidos[sonido].start();
+            rSonido = sonido;
+        }
+    }
+
+    public void setearFondoPantalla(View view){
+        int color = Integer.parseInt(view.getTag().toString());
+        switch (color){
+            case 1:
+                screen.setBackgroundColor(R.string.screen01);
+                break;
+            case 2:
+                screen.setBackgroundColor(R.string.screen02);
+                break;
+            case 3:
+                screen.setBackgroundColor(R.string.screen03);
+                break;
+            case 4:
+                screen.setBackgroundColor(R.string.screen04);
+                break;
+            case 5:
+                screen.setBackgroundColor(R.string.screen05);
+                break;
+            case 6:
+                screen.setBackgroundColor(R.string.screen06);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + color);
+        }
     }
 }
